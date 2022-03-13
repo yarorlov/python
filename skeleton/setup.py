69122ad2dbc0ffae04d0760e12e759c88f47676b
Yaroslav Orlov <yarorl42@gmail.com>
2022-03-13 07:59:18 +0300

    add skeleton directory

---
+++ skeleton/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Orlov Yaroslav',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'epoc@mail.ru',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+
+}
+
+setup(**config)
